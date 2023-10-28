fe4d997c4adbc9ebfaa4844bd42d533ea59ae663
Vika <victoria.gaponava@gmail.com>
2023-10-28 22:49:32 +0300

    described generics for lesson

---
+++ course/views/lesson.py
@@ -0,0 +1,28 @@
+from rest_framework import generics
+from rest_framework.generics import UpdateAPIView
+
+from course.models import Lesson
+from course.serializers.lesson import LessonSerializer
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
