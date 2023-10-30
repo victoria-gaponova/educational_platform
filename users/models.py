aa445e8ff86c976083bf0ba9f35b05582628d6a7
Vika <victoria.gaponava@gmail.com>
2023-10-30 23:34:15 +0300

    formatted models for users

--- users/models.py
+++ users/models.py
@@ -20,9 +20,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name="Почта")
     phone = models.CharField(max_length=35, verbose_name="Телефон")
     country = models.CharField(max_length=35, verbose_name="Страна")
-    avatar = models.ImageField(
-        upload_to="users/", null=True, blank=True, verbose_name="Аватар"
-    )
+    avatar = models.ImageField(upload_to="users/", null=True, blank=True, verbose_name="Аватар")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
