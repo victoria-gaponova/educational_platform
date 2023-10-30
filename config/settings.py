ee3ff34a3a9cd4beef58b63fe4da40cb132df0cd
Vika <victoria.gaponava@gmail.com>
2023-10-30 23:48:31 +0300

    installed django-filter

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
+    "django_filters"
+    
     "users",
     "course",
     "payments"
