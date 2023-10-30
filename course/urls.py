e04739b324be3f14cd0d474edda42688e0cb989c
Vika <victoria.gaponava@gmail.com>
2023-10-30 21:53:37 +0300

    formated urls for course

--- course/urls.py
+++ course/urls.py
@@ -22,7 +22,5 @@ urlpatterns = [
     path("lesson/", LessonListAPIView.as_view(), name="lesson-list"),
     path("lesson/<int:pk>", LessonRetrieveAPIView.as_view(), name="lesson-get"),
     path("lesson/update/<int:pk>", LessonUpdateAPIView.as_view(), name="lesson-update"),
-    path(
-        "lesson/delete/<int:pk>", LessonDestroyAPIView.as_view(), name="lesson-delete"
-    ),
+    path( "lesson/delete/<int:pk>", LessonDestroyAPIView.as_view(), name="lesson-delete"),
 ] + router.urls
