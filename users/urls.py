043d79811b284adee435929082a06adb82117266
Vika <victoria.gaponava@gmail.com>
2023-11-04 23:01:58 +0300

    add url for user

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,12 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
+from users.views import UserLoginViewSet
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', UserLoginViewSet.as_view({'post': 'create'}), name='user-login'),
 ]
