bb129a83c22017672765a3fe378c8bb95c740002
Vika <victoria.gaponava@gmail.com>
2023-11-04 20:30:16 +0300

    change urls course

--- course/urls.py
+++ course/urls.py
@@ -20,7 +20,7 @@ router.register(r"course", CourseViewSet, basename="course")
 urlpatterns = [
     path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
     path("lesson/", LessonListAPIView.as_view(), name="lesson-list"),
-    path("lesson/<int:pk>", LessonRetrieveAPIView.as_view(), name="lesson-get"),
-    path("lesson/update/<int:pk>", LessonUpdateAPIView.as_view(), name="lesson-update"),
-    path( "lesson/delete/<int:pk>", LessonDestroyAPIView.as_view(), name="lesson-delete"),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson-get"),
+    path("lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson-update"),
+    path( "lesson/delete/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson-delete"),
 ] + router.urls
