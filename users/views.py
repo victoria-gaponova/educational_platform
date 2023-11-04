40de2200d4b6def472d4392130438ffc6f1885c3
Vika <victoria.gaponava@gmail.com>
2023-11-04 23:03:46 +0300

    create view for user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import login
+from .serializers import UserLoginSerializer, UserSerializer
 
-# Create your views here.
+
+class UserLoginViewSet(viewsets.ViewSet):
+    """
+        Представление (ViewSet) для аутентификации пользователя и создания токена.
+
+        Attributes:
+            serializer_class (UserLoginSerializer): Класс сериализатора для аутентификации.
+
+        Methods:
+            create(request, *args, **kwargs): Метод для обработки запроса на аутентификацию пользователя,
+                создания токена и возврата данных пользователя вместе с токеном.
+
+        Returns:
+            Response: Ответ, содержащий токен и данные пользователя.
+    """
+    serializer_class = UserLoginSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+        login(request, user)
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        user_serializer = UserSerializer(user)
+        return Response({'token': token.key, 'user': user_serializer.data}, status=status.HTTP_200_OK)
