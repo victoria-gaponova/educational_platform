69b309b58760f62cc599214c6c5b9f7ab491192e
Vika <victoria.gaponava@gmail.com>
2023-11-04 20:32:19 +0300

    add access rights for lessons

--- course/views/lesson.py
+++ course/views/lesson.py
@@ -1,30 +1,35 @@
 from rest_framework import generics
-from rest_framework.generics import UpdateAPIView
-from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from course.models import Lesson
+from course.permissions import IsOwner, IsModerator
 from course.serializers.lesson import LessonSerializer
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
+
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated]
 
+
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner | IsAdminUser | IsModerator]
+
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner | IsAdminUser| IsModerator]
+
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner | IsAdminUser]
