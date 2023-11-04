dc3572b63ad66ffcc78c5fdd4cc3bd2fcd18239d
Vika <victoria.gaponava@gmail.com>
2023-11-04 20:32:08 +0300

    add access rights for course

--- course/views/course.py
+++ course/views/course.py
@@ -1,7 +1,8 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
 from course.models import Course
+from course.permissions import IsOwner, IsModerator
 from course.serializers.course import CourseSerializer
 
 
@@ -16,4 +17,18 @@ class CourseViewSet(ModelViewSet):
 
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        """Права доступа"""
+        if self.action == "retrieve":
+            permission_classes = [IsOwner | IsAdminUser | IsModerator]
+        elif self.action == "create":
+            permission_classes = [IsAdminUser]
+        elif self.action == "destroy":
+            permission_classes = [IsOwner | IsAdminUser]
+        elif self.action == "update":
+            permission_classes = [IsOwner | IsAdminUser| IsModerator]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
