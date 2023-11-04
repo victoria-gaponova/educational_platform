cfbbd276c50d41cfa3aca3a030bc1fac63f19e7d
Vika <victoria.gaponava@gmail.com>
2023-11-04 23:02:59 +0300

    add validator for url

---
+++ course/validators.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+
+def validator_scam_url(url):
+    """
+       Проверяет, является ли URL допустимым для использования.
+       Parameters:
+            - url (str): Проверяемый URL.
+       Raises:
+            - serializers.ValidationError: Вызывается, если URL не начинается с 'https://www.youtube.com/'.
+    """
+    if not url.startswith('https://www.youtube.com/'):
+        raise serializers.ValidationError('Использование стороннего ресурса!')
+
+
+
+
+
+
