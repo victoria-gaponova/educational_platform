13d0976315b5c3d075d347f0104ce16db3562e6f
Vika <victoria.gaponava@gmail.com>
2023-11-09 22:46:18 +0300

    formatted tests

--- course/tests.py
+++ course/tests.py
@@ -31,12 +31,16 @@ class LessonCRUDTests(APITestCase):
             'url': 'https://www.youtube.com/testlesson',
             'course': self.course.id
         }
+
         response = self.client.post(url, data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_retrieve_lesson(self):
         url = reverse('course:lesson-get', args=[self.lesson.id])
+
         response = self.client.get(url)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_lesson(self):
@@ -47,12 +51,16 @@ class LessonCRUDTests(APITestCase):
             'url': 'https://www.youtube.com/testlesson',
             'course': self.course.id
         }
+
         response = self.client.put(url, data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_lesson(self):
         url = reverse('course:lesson-delete', args=[self.lesson.id])
+
         response = self.client.delete(url)
+
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
@@ -68,13 +76,17 @@ class SubscriptionTests(APITestCase):
 
     def test_subscribe_to_course(self):
         url = reverse('course:subscribe-course', args=[self.course.id])
+
         response = self.client.post(url, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Subscription.objects.filter(user=self.user, course=self.course).exists())
 
     def test_unsubscribe_from_course(self):
         Subscription.objects.create(user=self.user, course=self.course)
         url = reverse('course:unsubscribe-course', args=[self.course.id])
+
         response = self.client.delete(url, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertFalse(Subscription.objects.filter(user=self.user, course=self.course).exists())
