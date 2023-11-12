df9e0cffd9c26fcc80080e9db975d0d092d2be5d
Vika <victoria.gaponava@gmail.com>
2023-11-12 19:21:43 +0300

    change payments views

--- payments/views.py
+++ payments/views.py
@@ -1,15 +1,16 @@
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 from payments.models import Payment
 from payments.serializers import PaymentSerializer
 
 
-class PaymentListViewSet(generics.ListAPIView):
+class PaymentViewSet(ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['course', 'lesson', 'payment_method']
-    ordering_fields = ['date',]
+    ordering_fields = ['date', ]
     permission_classes = [IsAuthenticated]
