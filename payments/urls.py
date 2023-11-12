89d2eeb4007d9375019a236829d1830461343983
Vika <victoria.gaponava@gmail.com>
2023-11-12 19:22:02 +0300

    change payments urls

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from payments.apps import PaymentsConfig
-from payments.views import PaymentListViewSet
+from payments.views import PaymentViewSet
 
 app_name = PaymentsConfig.name
+router = DefaultRouter()
+router.register(r"payment", PaymentViewSet, basename="payment")
 
 urlpatterns = [
-    path('payments/', PaymentListViewSet.as_view(), name='payments-list')
-]
+              ] + router.urls
